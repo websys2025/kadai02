51e5f7cf4bd39adc63c537a9586e0d96f1bf1916
j23097 落合 直智 <j23097on@edu.tuis.ac.jp>
2025-04-21 11:46:25 +0900

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,9 +1,14 @@
 import random
+import statistics                                #平均値を求める関数をインポート
 
+
+l = []                                           #平均値を求めるためのリストを定義
 for i in range(10):
     x = random.randint(1,6)
     print(str(i+1)+"回目："+str(x))
-
+    l.append(x)                                  #リストに値を追加
+else:
+    print("平均値："+ str(statistics.mean(l)))    #平均値の結果を表示
 # 期待される出力結果例
 """
 1回目：4

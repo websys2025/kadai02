073118fbf1dfcd2be4d3a13a43ae020b649501f9
j23126ky <j23126ky@edu.tuis.ac.jp>
2025-04-21 11:05:56 +0900

    add:平均値を出すコードを追加

--- dice.py
+++ dice.py
@@ -1,8 +1,14 @@
 import random
 
+total = 0
+
 for i in range(10):
-    x = random.randint(1,6)
-    print(str(i+1)+"回目："+str(x))
+    x = random.randint(1, 6)
+    total += x
+    print(str(i + 1) + "回目：" + str(x))
+
+average = total / 10
+print("平均値：" + str(average))
 
 # 期待される出力結果例
 """
